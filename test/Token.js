8fc3f2add6f54009c36414bb9d3c3568e160d833
orkarfabianthewise <orkarmelch@gmail.com>
2024-06-05 11:18:12 +0100

    5: smoke testing contracts

--- test/Token.js
+++ test/Token.js
@@ -1,17 +1,54 @@
 const {expect} = require("chai");
 const {ethers} = require("hardhat");
 
+const tokens = (n)=> {
+	return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
 describe("Token", ()=> {
 	// Tests go here
-	it("has a name", async ()=> {
+	let token
+	// Avoid duplicates
+	beforeEach(async ()=> {
 		// fetch Token from blkchain
 		const Token = await ethers.getContractFactory("Token")
 		// deployed instance
-		let token = await Token.deploy()
-		// Read token name
-		const name = await token.name()
-		// check name is correct
-		console.log(`Token Name: ${name}`)
-		expect(name).to.equal("My Token")
+		token = await Token.deploy("Dapp University", "DAPP", '1000000')
 	})
+
+	describe("Deployment", ()=> {
+        const name = "Dapp University"
+        const symbol = "DAPP"
+        const decimals = "18"
+        const totalSupply = tokens("1000000")
+		it("has a name", async ()=> {
+			console.log(`Token Name: ${name}`)
+			expect(await token.name()).to.equal("Dapp University")
+		  })
+
+	    it("has correct symbol", async ()=> {
+			// Read token symbol
+			// check symbol is correct
+			expect(await token.symbol()).to.equal("DAPP")
+		})
+
+		it("has correct decimals", async ()=> {
+			// Read token decimals
+			// check decimals is correct
+			expect(await token.decimals()).to.equal(18)
+		})
+
+		it("has correct totalSupply", async ()=> {
+			// Read token totalSupply
+			// check totalSupply is correct
+			expect(await token.totalSupply()).to.equal(tokens('1000000'))
+		})
+	})
+
+	// Describe Spending...
+
+	// Describe approving...
+
+	// Describe ...
+
 })
